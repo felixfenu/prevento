652e2443fdcf19aceb4e2b658f0b8b183326ec60
felixfenu <ext_fefenuqu@mercadolibre.com>
2022-11-29 20:50:11 -0300

    mysql dialect

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -14,7 +14,7 @@ module.exports = {
     "password": process.env.DB_PASSWORD,
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT,
+    "dialect": "mysql",
     "port":process.env.DB_PORT
   },
 }
