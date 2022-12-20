c99a34ec8b2e403ad4a1284cd4f3720dc130b84f
lucasTronc <lucasmurcielago2@hotmail.com.ar>
2022-12-20 19:48:49 -0300

    validacion front 3

---
+++ public/javascript/editar.js
@@ -0,0 +1,33 @@
+
+let f = document.getElementById("formu");
+
+f.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    if (name==""){
+        alert("completar el campo nombre");
+        // aca corta con el retun
+        return;
+    }
+
+    // listo
+    let price = document.getElementById("price").value;
+    if (price==""){
+        alert("completar el campo precio");
+        // aca corta con el retun
+        return;
+    }
+
+    let description = document.getElementById("description").value;
+    if (description==""){
+        alert("completar el campo descripcion");
+        // aca corta con el retun
+        return;
+    }
+
+    f.submit();
+
+    
+})
